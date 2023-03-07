f47c263dd7ba13acba405b41e68ad06435dbc97b
Murillo Morgado <mumorgadok@hotmail.com>
2023-03-06 21:50:15 -0300

    altecao

---
+++ script.js
@@ -0,0 +1,12 @@
+/* let show = true;
+
+const menuSection = document.querySelector(".ok")
+const menuToogle = document.querySelector("#menu-toggle")
+
+menuToogle.addEventListener("click", () => {
+
+    document.body.style.overflow = show ? "hidden" : "initial"
+
+    menuSection.classList.ok("on", show)
+    show = !show;
+}) */
