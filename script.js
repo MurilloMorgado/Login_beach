5fd04cc7d0db4571c508b648a2d816c6561d1322
Murillo Morgado <mumorgadok@hotmail.com>
2023-03-07 08:34:44 -0300

    iniciando responsividade no nav

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-/* let show = true;
+let show = true;
 
-const menuSection = document.querySelector(".ok")
-const menuToogle = document.querySelector("#menu-toggle")
+const menuSection = document.querySelector(".menu-section")
+const menuToogle = document.querySelector(".menu-toggle")
 
 menuToogle.addEventListener("click", () => {
-
     document.body.style.overflow = show ? "hidden" : "initial"
 
-    menuSection.classList.ok("on", show)
+    menuSection.classList.toggle("on", show)
     show = !show;
-}) */
+})
